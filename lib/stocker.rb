be2f488702c9c525bf150c84a99fa0974dbf3fa9
Ted Sazon <tedsazon@gmail.com>
2019-04-23 04:04:01 +0000

    stocker work

--- lib/stocker.rb
+++ lib/stocker.rb
@@ -1,6 +1,23 @@
 require "stocker/version"
+require "open-uri"
 
-module Stocker
-  class Error < StandardError; end
-  # Your code goes here...
-end
+class stocker
+
+  def self.scrape_page(index_url)
+    index_page = Nokogiri::HTML(open(index_url))
+    sneakers = []
+    index_page.css("div.container").each do |card|
+      card.css("").each do |sneaker|
+        sneaker_link = "#{sneaker.attr('href')}"
+        sneaker_name = sneaker.css('').text
+        sneaker_lowest_ask = sneaker.css('').text
+        sneakers << {name: sneaker_name, lowest_ask: sneaker_lowest_ask, url: sneaker_link}
+      end
+    end
+    sneakers
+  end
+
+  def self.scrape_sneaker(sneaker_url)
+    sneaker = {}
+    sneaker_page = Nokogiri::HTML(open(sneaker_url))
+    links = profile_page.css("")
